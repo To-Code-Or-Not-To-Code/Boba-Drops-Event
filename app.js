ae4ec4c457372b49b665a7e9ad3c81ef1255dd45
Rick Zhan <rz.codestudio@gmail.com>
2026-03-06 20:49:22 +0000

    Add page animations

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ let links = document.querySelectorAll("a");
 if (links) {
 
     // Go through each of them
-    links.forEach(function (link) {
-
+    for (let i = 0; i < links.length; i++) {
+        let link = links[i];
         // Add an onclick event listener
         link.addEventListener("click", function (e) {
             // Grab body
@@ -16,6 +16,9 @@ if (links) {
 
             // Prevent navigation
             e.preventDefault();
+
+            // Add fade out
+            body.classList.add("fade-out")
 
             // Run a function that conditionally redirects after 500ms
             setTimeout(function () {
@@ -29,78 +32,6 @@ if (links) {
                     console.error("Whaaaaaa")
                 }
             }, 500);
-
-            // Add fade out
-            body.classList.add("fade-out")
-        });
-    });
-}
-
-
-let throttling = false;
-
-function onScrollThrottled() {
-    if (!throttling) {
-        throttling = true;
-        window.requestAnimationFrame(() => {
-            onScroll();
-            throttling = false;
         });
     }
 }
-
-
-let navbarTop = 0;
-let transition = true;
-let position = "absolute";
-let lastScrollPosition = 0;
-
-let navar = document.querySelector("nav");
-
-function onScroll() {
-
-    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScrollPosition <= 0) {
-
-        lastScrollPosition = 0;
-        navbarTop = 0;
-
-        if (position !== "absolute") { transition = true; }
-        else { transition = false; }
-        position = "absolute";
-
-    } else {
-
-        if (currentScrollPosition > lastScrollPosition) {
-
-            if (position !== "absolute") { transition = true; }
-            else { transition = false; }
-            position = "absolute";
-
-            let { top, height } = navbar.getBoundingClientRect()
-            navbarTop = currentScrollPosition + Math.max(top, -height);
-
-        } else {
-
-            const { top } = navbar.getBoundingClientRect()
-
-            if (top >= 0) {
-
-                navbarTop = 0;
-
-                if (position !== "fixed") { transition = true; }
-                else { transition = false; }
-                position = "fixed";
-
-            }
-
-        }
-
-        lastScrollPosition = currentScrollPosition;
-
-    }
-    navbar.style = `position: ${position}; top: ${navbarTop}px; transition: ${transition ? "none" : "100ms linear"}`;
-}
-
-window.addEventListener("scroll", onScrollThrottled, { passive: true });
